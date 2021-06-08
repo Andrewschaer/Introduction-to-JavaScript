f1ecdb8b42b0bda9af0d95cc0ca98a68d2cc49e7
Andrewschaer <andrewschaer@github.com>
2021-06-08 19:35:51 -0400

    updated answers for task 5

--- index.js
+++ index.js
@@ -191,9 +191,11 @@ Using the miles function below do the following:
   3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-    /*add your code here*/
+function miles(km){
+    return km * 0.621371
   }
+
+console.log(miles(7))
 
 
 
@@ -205,10 +207,11 @@ Using the feet function below do the following:
   3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-    /*add your code here*/
+function feet(cm){
+   return cm / 30.48
   }
  
+console.log(feet(40))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
